59d1506d94eb9d8aa7493751d30af4c121674f8c
sridhar <sridharreddykolam007@gmail>
2025-07-17 13:40:11 -0500

    Updated add class

--- Add.java
+++ Add.java
@@ -1,2 +1,16 @@
 public class Add {
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, World!");
+
+        // Declare two numbers
+        int a = 10;
+        int b = 20;
+
+        // Add the numbers
+        int sum = a + b;
+
+        // Print the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
 }
